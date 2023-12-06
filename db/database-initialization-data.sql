9561c53346105236f00031ededf1b2035da6434d
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2023-12-06 12:18:44 +0800

    further db schema refinements

---
+++ db/database-initialization-data.sql
@@ -0,0 +1,32 @@
+insert into userlevels(userlevel, leveldesc, creationdate) values(1, 'Citizen', NOW());
+insert into userlevels(userlevel, leveldesc, creationdate) values(2, 'Enumerator', NOW());
+insert into userlevels(userlevel, leveldesc, creationdate) values(3, 'Administrator', NOW());
+insert into userlevels(userlevel, leveldesc, creationdate) values(4, 'Super Administrator', NOW());
+
+insert into regions(regionname, creationdate) values('National Capital Region (NCR)', NOW());
+insert into regions(regionname, creationdate) values('Region 1 - Ilocos Region', NOW());
+insert into regions(regionname, creationdate) values('Region 2 - Cagayan Valley', NOW());
+insert into regions(regionname, creationdate) values('Region 3 - Central Luzon', NOW());
+insert into regions(regionname, creationdate) values('Region 4A - CALABARZON', NOW());
+insert into regions(regionname, creationdate) values('Region 4B - MIMAROPA', NOW());
+insert into regions(regionname, creationdate) values('Region 5 - Bicol Region', NOW());
+insert into regions(regionname, creationdate) values('Region 6 - Western Visayas', NOW());
+insert into regions(regionname, creationdate) values('Region 7 - Central Visayas', NOW());
+insert into regions(regionname, creationdate) values('Region 8 - Eastern Visayas', NOW());
+insert into regions(regionname, creationdate) values('Region 9 - Zamboanga Peninsula', NOW());
+insert into regions(regionname, creationdate) values('Region 10 - Northern Mindanao', NOW());
+insert into regions(regionname, creationdate) values('Region 11 - Davao Region', NOW());
+insert into regions(regionname, creationdate) values('Region 12 - SOCCSKSARGEN', NOW());
+insert into regions(regionname, creationdate) values('Region 13 - CARAGA Administrative Region', NOW());
+insert into regions(regionname, creationdate) values('Cordillera Administrative Region', NOW());
+insert into regions(regionname, creationdate) values('Autonomous Region in Muslim Mindanao', NOW());
+
+insert into genders(genderdesc) values("Male");
+insert into genders(genderdesc) values("Female");
+insert into genders(genderdesc) values("LGBTQ+");
+
+insert into civstatus(civstatusdesc) values("Single");
+insert into civstatus(civstatusdesc) values("Married");
+insert into civstatus(civstatusdesc) values("Separated");
+insert into civstatus(civstatusdesc) values("Widower");
+
--- db/database-initialization.sql
+++ db/database-initialization.sql
@@ -1,4 +1,4 @@
-create table userlevels (
+create table [if not exists] userlevels (
 	id serial primary key,
 	userlevel integer unique check (userlevel < 4),
 	leveldesc varchar(64) not null,
@@ -7,7 +7,7 @@ create table userlevels (
 
 create index idx_userlevel on userlevels(id, userlevel);
 
-create table regions (
+create table [if not exists] regions (
 	id serial primary key,
 	regionname varchar(64) not null,
 	creationdate timestamptz not null	
@@ -15,7 +15,7 @@ create table regions (
 
 create index idx_region on regions(id, regionname);
 
-create table provinces (
+create table [if not exists] provinces (
 	id serial primary key,
 	provincename varchar(64) not null,
 	regionid integer references regions(id) on delete restrict,
@@ -24,7 +24,7 @@ create table provinces (
 
 create index idx_province on provinces(id, provincename);
 
-create table citymunicipality (
+create table [if not exists] citymunicipality (
 	id serial primary key,
 	towncitymunicipalityname varchar(64) not null,
 	iscity boolean default false not null,
@@ -34,7 +34,7 @@ create table citymunicipality (
 
 create index idx_citymunicipality on citymunicipality(id, towncitymunicipalityname);
 
-create table barangays (
+create table [if not exists] barangays (
 	id serial primary key,
 	barangayname varchar(64) not null,
 	citymunicipalityid integer references citymunicipality(id) on delete restrict,
@@ -43,29 +43,51 @@ create table barangays (
 
 create index idx_barangay on barangays(id, barangayname);
 
-create table systemusers (
+create table [if not exists] genders (
+	id serial primary key,
+	genderdesc varchar(64) not null,
+	creationdate timestamptz not null
+);
+
+create index idx_gender on genders(id,genderdesc);
+
+create table [if not exists] civstatus (
+  id serial primary key,
+  civstatusdesc varchar(32) not null
+);
+
+create table [if not exists] users (
 	id serial primary key,
 	lastname varchar(64) not null,
 	firstname varchar(64) not null,
 	midname varchar(5) default null,
 	extname varchar(5) default null,
-	position varchar(64) default null,
+	birthdate date default null,
 	contactlandline varchar(128) default null,
 	contactmobile varchar(128) default null,
 	contactemail varchar(128) default null,
-	provinceid integer default null references provinces(id) on delete restrict,
-	citymunicipalityid integer default null references citymunicipality(id) on delete restrict,
-	barangayid integer default null references barangays(id) on delete restrict,
+	genderId integer default null references genders(id) on delete restrict,
+	civstatusId integer default null references civstatus(id) on delete restrict,
 	usrname varchar(128) unique not null,
 	usrpassword varchar(128) not null,
 	usrpasswdsalt varchar(128) not null,
 	userlevelid integer default 1 references userlevels(userlevel) on delete restrict check(userlevelid < 4),
 	isapproved boolean default false,
-	approvedby integer default null references systemusers(id) on delete restrict,
+	approvedby integer default null references users(id) on delete restrict,
 	approveddate timestamptz default null,
-	createdby integer default null references systemusers(id) on delete restrict,
+	createdby integer default null references users(id) on delete restrict,
 	creationdate timestamptz default null
 );
 
-create index idx_systemuser on systemusers(id, lastname, firstname, midname, extname, usrname, usrpassword);
+create index idx_user on users(id, lastname, firstname, midname, extname, usrname, usrpassword);
 
+create table [if not exists] useraddress (
+	id serial primary key,
+	userId integer not null references users(id) on delete restrict,
+	provinceid integer default null references provinces(id) on delete restrict,
+	citymunicipalityid integer default null references citymunicipality(id) on delete restrict,
+	barangayid integer default null references barangays(id) on delete restrict,
+	streetname varchar(64) default null,
+	doorfloorbldghousenumber varchar(128) default null,
+	creationdate timestamptz default null	
+);
