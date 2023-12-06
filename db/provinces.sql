5342a99dec48bd34953115f8a9080551f51e33bd
Tito Mari Francis Escano <titomarifrancis@gmail.com>
2023-12-06 12:48:50 +0800

    added provinces, cities, municipalities and barangays

---
+++ db/provinces.sql
@@ -0,0 +1,115 @@
+--ncr
+insert into provinces (provincename, regionid, creationdate) values('Metro Manila', 1, NOW());
+
+--region 1
+insert into provinces (provincename, regionid, creationdate) values('Ilocos Norte', 2, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Ilocos Sur', 2, NOW());
+insert into provinces (provincename, regionid, creationdate) values('La Union', 2, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Pangasinan', 2, NOW());
+
+--region 2
+insert into provinces (provincename, regionid, creationdate) values('Batanes', 3, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Cagayan', 3, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Isabela', 3, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Nueva Vizcaya', 3, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Quirino', 3, NOW());
+
+--region 3
+insert into provinces (provincename, regionid, creationdate) values('Aurora', 4, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Bataan', 4, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Bulacan', 4, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Nueva Ecija', 4, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Pampanga', 4, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Tarlac', 4, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Zambales', 4, NOW());
+
+--region 4A
+insert into provinces (provincename, regionid, creationdate) values('Batangas', 5, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Cavite', 5, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Laguna', 5, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Quezon', 5, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Rizal', 5, NOW());
+
+--mimaropa
+insert into provinces (provincename, regionid, creationdate) values('Marinduque', 6, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Occidental Mindoro', 6, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Oriental Mindoro', 6, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Palawan', 6, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Romblon', 6, NOW());
+
+--region 5
+insert into provinces (provincename, regionid, creationdate) values('Albay', 7, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Camarines Norte', 7, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Camarines Sur', 7, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Catanduanes', 7, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Masbate', 7, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Sorsogon', 7, NOW());
+
+--region 6
+insert into provinces (provincename, regionid, creationdate) values('Aklan', 8, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Antique', 8, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Capiz', 8, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Guimaras', 8, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Iloilo', 8, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Negros Occidental', 8, NOW());
+
+--region 7
+insert into provinces (provincename, regionid, creationdate) values('Bohol', 9, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Cebu', 9, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Negros Oriental', 9, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Siquijor', 9, NOW());
+
+--region 8
+insert into provinces (provincename, regionid, creationdate) values('Biliran', 10, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Eastern samar', 10, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Leyte', 10, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Northern Samar', 10, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Samar', 10, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Southern Leyte', 10, NOW());
+
+--region 9
+insert into provinces (provincename, regionid, creationdate) values('Zamboanga del Norte', 11, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Zamboanga del Sur', 11, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Zamboanga Sibugay', 11, NOW());
+
+--region 10
+insert into provinces (provincename, regionid, creationdate) values('Bukidnon', 12, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Camiguin', 12, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Lanao del Norte', 12, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Misamis Occidental', 12, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Misamis Oriental', 12, NOW());
+
+--region 11
+insert into provinces (provincename, regionid, creationdate) values('Compostela Valley', 13, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Davao del Norte', 13, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Davao del Sur', 13, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Davao Occidental', 13, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Davao Oriental', 13, NOW());
+
+--region 12
+insert into provinces (provincename, regionid, creationdate) values('North Cotabato', 14, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Sarangani', 14, NOW());
+insert into provinces (provincename, regionid, creationdate) values('South Cotabato', 14, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Sultan Kudarat', 14, NOW());
+
+--region 13 CARAGA
+insert into provinces (provincename, regionid, creationdate) values('Agusan del Norte', 15, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Agusan del Sur', 15, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Dinagat Islands', 15, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Surigao del Norte', 15, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Surigao del Sur', 15, NOW());
+
+--cordillera
+insert into provinces (provincename, regionid, creationdate) values('Abra', 16, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Apayao', 16, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Benguet', 16, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Ifugao', 16, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Kalinga', 16, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Mt. Province', 16, NOW());
+
+--ARMM
+insert into provinces (provincename, regionid, creationdate) values('Basilan', 17, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Lanao del Sur', 17, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Maguindanao', 17, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Sulu', 17, NOW());
+insert into provinces (provincename, regionid, creationdate) values('Tawi-Tawi', 17, NOW());
